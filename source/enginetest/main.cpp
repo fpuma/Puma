cfb428e77ecea981052a1d40bc8f9ddc9d62e331
fpuma <fpuma88@gmail.com>
2020-11-26 19:51:36 +0100

    Initial test

--- source/enginetest/main.cpp
+++ source/enginetest/main.cpp
@@ -1,4 +1,28 @@
-int main()
+#include <precompiledengine.h>
+
+#include <engine/iengine.h>
+#include <engine/services/igraphicsservice.h>
+
+#include <graphics/graphicdefinitions.h>
+
+using namespace puma;
+
+int main( int argc, char* argv[] )
 {
+
+    auto enginePtr = IEngine::create();
+    enginePtr->init();
+
+    gfx::Extent windowExtent = { 500,500,100,100 };
+    gGraphics->init( windowExtent, "EngineTest" );
+
+    while ( !enginePtr->shouldQuit() )
+    {
+        enginePtr->update();
+        enginePtr->render();
+    };
+
+    enginePtr->uninit();
+
     return 0;
 }
